e03afb817564708027bf19173cd1be2f19ef73bf
Franco Rabaglia <francoo.hd@gmail.com>
2016-09-20 20:15:33 -0300

    connection

--- model/db.js
+++ model/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/dcudb');
+mongoose.connect('mongodb://heroku_0n621108:hgsolivj50plo8bvbmckme8sa9@ds035856.mlab.com:35856/heroku_0n621108');
